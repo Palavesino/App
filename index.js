31ee41dfac1b677ce93c9e017d31084766f15768
Facundo <palavesinofacundo@gmail.com>
2024-11-28 16:13:50 -0300

    base64

--- index.js
+++ index.js
@@ -81,19 +81,49 @@ app.get('/start-bot', async (req, res) => {
   }
 });
 
+// Función para convertir un archivo en base64
+const convertToBase64 = (filePath) => {
+    try {
+        const file = fs.readFileSync(filePath);  // Lee el archivo de forma síncrona
+        return file.toString('base64');  // Convierte a Base64
+    } catch (error) {
+        console.error("Error al leer el archivo:", error);
+        throw error;
+    }
+};
+
+
+// app.get('/get-qr', async (req, res) => {
+//   try {
+//     console.log("Generando qr");
+//     const imagePath = path.join(process.cwd(), 'bot.qr.png');
+//     const image = fs.createReadStream(imagePath);
+//     res.setHeader('Content-Type', 'image/png');
+//     res.setHeader('Content-Disposition', 'attachment; filename="bot.qr.png"');
+//     image.pipe(res);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Ocurrió un error al iniciar el bot.' });
+//   }
+// });
+
 app.get('/get-qr', async (req, res) => {
   try {
     console.log("Generando qr");
+    // Ruta al archivo PNG generado
     const imagePath = path.join(process.cwd(), 'bot.qr.png');
-    const image = fs.createReadStream(imagePath);
-    res.setHeader('Content-Type', 'image/png');
-    res.setHeader('Content-Disposition', 'attachment; filename="bot.qr.png"');
-    image.pipe(res);
+
+    // Convierte la imagen a base64
+    const imageBase64 = convertToBase64(imagePath);
+
+    // Devuelve la imagen en base64 y un mensaje
+    res.status(200).json({
+        message: 'qr generado correctamente.',
+        imageBase64: imageBase64,
+    });
   } catch (error) {
     res.status(500).json({ error: 'Ocurrió un error al iniciar el bot.' });
   }
 });
-
 // Arrancar el servidor de Express
 app.listen(port, () => {
   console.log(`Servidor Express corriendo en http://localhost:${port}`);

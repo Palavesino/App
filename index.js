6d1c27b9d3dcbf9ee11f7a8369c4c2ebb0870957
Facundo <palavesinofacundo@gmail.com>
2024-12-08 01:04:02 -0300

    comment memory

--- index.js
+++ index.js
@@ -37,14 +37,14 @@ const main = async () => {
 };
 
 // Monitoreo del uso de memoria
-setInterval(() => {
-  const memoryUsage = process.memoryUsage();
-  console.log("Uso de memoria:");
-  console.log(`  RSS: ${(memoryUsage.rss / 1024 / 1024).toFixed(2)} MB`);
-  console.log(`  Heap Total: ${(memoryUsage.heapTotal / 1024 / 1024).toFixed(2)} MB`);
-  console.log(`  Heap Used: ${(memoryUsage.heapUsed / 1024 / 1024).toFixed(2)} MB`);
-  console.log(`  External: ${(memoryUsage.external / 1024 / 1024).toFixed(2)} MB`);
-}, 10000); // Cada 10 segundos
+// setInterval(() => {
+//   const memoryUsage = process.memoryUsage();
+//   console.log("Uso de memoria:");
+//   console.log(`  RSS: ${(memoryUsage.rss / 1024 / 1024).toFixed(2)} MB`);
+//   console.log(`  Heap Total: ${(memoryUsage.heapTotal / 1024 / 1024).toFixed(2)} MB`);
+//   console.log(`  Heap Used: ${(memoryUsage.heapUsed / 1024 / 1024).toFixed(2)} MB`);
+//   console.log(`  External: ${(memoryUsage.external / 1024 / 1024).toFixed(2)} MB`);
+// }, 10000); // Cada 10 segundos
 
 app.get("/", (req, res) => {
   const htmlResponse = `
